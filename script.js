66bf987925f9c7680f86ffaa820adde76723537a
Abhay <abhayagrahari52@gmail.com>
2025-04-08 16:30:09 +0530

    formatting the code

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 //Initialzing gsap
-let tl = gsap.timeline()
-let section3Ele = document.querySelector('.section3').children
+let tl = gsap.timeline();
+let section3Ele = document.querySelector(".section3").children;
 
 //Adding animations to section1 of page
-tl.from('.section1 h2',{
-    opacity:0,
-    y:150,
-    
-    duration:1,
-})
-tl.from('.section1 .content',{
-    opacity:0,
-    y:100,
-    duration:1,
-})
+tl.from(".section1 h2", {
+  opacity: 0,
+  y: 150,
 
-gsap.from('.section2',{
-    opacity:0,
-    y:200,
-    scrollTrigger:{
-        trigger:'.section2',
-        start:"top 60%",
-        end:'top 20%',
-        // markers:true,
-        scrub:true,
-        pin:true
-    },
-    duration:1
+  duration: 1,
+});
+tl.from(".section1 .content", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+});
 
-})
+gsap.from(".section2", {
+  opacity: 0,
+  y: 200,
+  scrollTrigger: {
+    trigger: ".section2",
+    start: "top 60%",
+    end: "top 20%",
+    // markers:true,
+    scrub: true,
+    pin: true,
+  },
+  duration: 1,
+});
 for (let index = 0; index < section3Ele.length; index++) {
-    const element = section3Ele[index];
-    gsap.to(`.${element.className}`,{
-        opacity:0,
-        transform:"Scale(.7)",
-        scrollTrigger:{
-            trigger:`.${element.className}`,
-            start:"top 5%",
-            end:"bottom 15%",
-            // markers:true,
-            // pin:true,
-            scrub:true,
-            staggered:1
-        }
-    })
-    
+  const element = section3Ele[index];
+  gsap.to(`.${element.className}`, {
+    opacity: 0,
+    transform: "Scale(.7)",
+    scrollTrigger: {
+      trigger: `.${element.className}`,
+      start: "top 5%",
+      end: "bottom 15%",
+      // markers:true,
+      // pin:true,
+      scrub: true,
+      staggered: 1,
+    },
+  });
 }
