667d035c2dcb0eba42093e253595bc5bc65cb4ea
Abhay <abhayagrahari52@gmail.com>
2025-04-08 08:00:25 +0530

    Added section 2 in body:Meet our team

--- script.js
+++ script.js
@@ -4,11 +4,26 @@ let tl = gsap.timeline()
 //Adding animations to section1 of page
 tl.from('.section1 h2',{
     opacity:0,
-    y:100,
+    y:150,
+    
     duration:1,
 })
 tl.from('.section1 .content',{
     opacity:0,
     y:100,
     duration:1,
+})
+
+gsap.from('.section2',{
+    opacity:0,
+    transform:"Scale(.95)",
+    y:200,
+    scrollTrigger:{
+        trigger:'.section2',
+        start:"top 60%",
+        end:'top 40%',
+        markers:true,
+        scrub:true
+    },
+
 })
