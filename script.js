a528d09993b05cb1bcd444381d2a0c8155c8716d
Abhay <abhayagrahari52@gmail.com>
2025-04-16 11:35:30 +0530

    Merging all branches and fixing conflicts

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ gsap.from(".section2", {
   y: 300,
   scrollTrigger: {
     trigger: ".section2",
-    start: "top 50%",
-    end: "top 45%",
+    start: "top 60%",
+    end: "top 55%",
     // markers:true,
     scrub: true,
     // pin: true,
